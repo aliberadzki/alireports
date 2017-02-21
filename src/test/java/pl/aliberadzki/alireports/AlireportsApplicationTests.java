a3574367c87245fa640e675879ab164327ed30b8
Adam Liberadzki <adam.liberadzki@gmail.com>
2017-02-21 23:04:50 +0100

    init spring boot

---
+++ src/test/java/pl/aliberadzki/alireports/AlireportsApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.aliberadzki.alireports;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlireportsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
